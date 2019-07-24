912457305907ec245f0e780ded5c8a0495ee8c1d
Isma <ismael.c.26a@gmail.com>
2019-07-24 05:22:23 -0400

    Se creó el primer componente

---
+++ src/components/Badge.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Badge extends React.Component{
+    render(){
+        return (
+            <h1>Este es mi primer componente</h1>
+        )
+    }
+
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = "Ismael Cuevas";
-const element = (
-    <div>
-        <h1>Hello, My name is {name}</h1>
-        <p>Estoy aprendiendo REACT JS</p>
-    </div>
-);
+// Importando nuestro primer componente
+import Badge from './components/Badge'
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge/>, container);
 
-/* **** CON React.createElement ****
-const element = React.createElement(
-    'div',
-    {},
-    React.createElement('h1',{},`Hello, My name is ${name}`),
-    React.createElement('h1',{},`Estoy aprendiendo REACT JS`),
-    );
-
-***** CON JSX *****
-
-const element = (
-    <div>
-        <h1>Hello, My name is {name}</h1>
-        <p>Estoy aprendiendo REACT JS</p>
-    </div>
-);
-
-*/
