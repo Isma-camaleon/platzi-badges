2dbc64de5f4ae15dedb0cd18934208528db7ff8b
Isma <ismael.c.26a@gmail.com>
2019-07-24 05:38:08 -0400

    03.Primer_componente Se crea un bosquejo de un badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from 'react';
+import confLogo from '../images/badge-header.svg';
 
-class Badge extends React.Component{
-    render(){
-        return (
-            <h1>Este es mi primer componente</h1>
-        )
-    }
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
 
+        <div>
+          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+          <h1>
+            Ismael <br /> Cuevas
+          </h1>
+        </div>
+
+        <div>
+          <p>Frontend Engineer</p>
+          <p>@is.cuevas</p>
+        </div>
+
+        <div>#platziconf</div>
+      </div>
+    );
+  }
 }
 
 export default Badge;
