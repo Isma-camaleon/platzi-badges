7e3b79fbe0ccf4e43bd0833c1301988ad0030548
Isma <ismael.c.26a@gmail.com>
2019-07-24 04:45:46 -0400

    Diferenciamos JSX con React.createElement
    Primer ejemplo de la diferencia de usar JSX y React.createElement

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+//const element = <h1>Hello, Platzi Badges!</h1>;
+const element = React.createElement('h1',{},'Hola, Soy los childen.');
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(element, container);
+
+/* **** SIN JSX ****
+const element = React.createElement(__element__ , __atributos__ , __content__);
+const element = React.createElement('h1',{},'Hola, Soy los childen.');
+const element = React.createElement('a',{ href:'www.platzi.com' },'Ir a Platzi');
+*/
